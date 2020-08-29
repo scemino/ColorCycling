537de00221c00c7e3a3910f496a73dc4ffd4d02b
scemino <scemino74@gmail.com>
2020-08-29 13:48:34 +0200

    Oops remove the mandatory application argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include "ColorCyclingApplication.h"
 #include <iostream>
 
-static void usage(const char* exe) {
-  std::cout << "usage: " << exe << " file.lbm" << std::endl;
-}
-
 int main(int argc, const char **argv) {
-  if (argc != 2) {
-    usage(argv[0]);
-    return EXIT_SUCCESS;
-  }
-
   ColorCyclingApplication app;
   app.run();
   return EXIT_SUCCESS;
